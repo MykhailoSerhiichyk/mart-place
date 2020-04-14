bdd4fb492f2db8d9038ab77674fb93159ea94b14
StreetDream <mishkasaltovskiy@gmail.com>
2020-04-14 17:12:26 +0300

    added category page

--- app/js/main.js
+++ app/js/main.js
@@ -9,15 +9,35 @@ $(function(){
     $('.product-slider__inner').slick({
 
     });
-    
-    var mixer = mixitup('.product-mixitup__inner-box');
 
     $('.folowers-slider__inner').slick({
         slidesToShow: 3,
         slidesToScroll: 3
     });
 
-    $('select').styler();
+    $('.search select').styler();
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 320,
+        from: 50,
+        to: 200,
+        prefix: "$"
+    });
+
+    $('.lnr-list').on('click', function(){
+        $('.category__content-item').addClass('list');
+        $('.lnr-list').addClass('active');
+        $('.lnr-frame-expand').removeClass('active');
+    });
+
+    $('.lnr-frame-expand').on('click', function(){
+        $('.category__content-item').removeClass('list');
+        $('.lnr-frame-expand').addClass('active');
+        $('.lnr-list').removeClass('active');
+    });
+
+    var mixer = mixitup('.product-mixitup__inner-box');
 
 });
